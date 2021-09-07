4f7e70eb6486c9859d5b353022f549457940765f
mujtabahs <mujtaba.hussein@gmail.com>
2021-09-07 18:05:00 +0500

    Update views.py

--- xurlapi/views.py
+++ xurlapi/views.py
@@ -6,6 +6,7 @@ from rest_framework import viewsets
 from .serializers import HeroSerializer
 from .models import Hero
 
+@csrf_exempt
 class HeroViewSet(viewsets.ModelViewSet):
     queryset = Hero.objects.all().order_by('name')
     serializer_class = HeroSerializer
